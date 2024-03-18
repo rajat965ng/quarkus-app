c4b4a7097f73d02b112f61dc2653eec540fe1f08
Rajat Nigam <rajatnigam89@gmail.com>
2024-03-18 13:20:30 +0530

    update with blue-green deployment

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,13 +4,28 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "greeting.message.v1")
+    private String message_v1;
+
+    @ConfigProperty(name = "greeting.message.v2")
+    private String message_v2;
+
     @GET
+    @Path("/v1")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public String helloV1() {
+        return message_v1;
     }
+    @GET
+    @Path("/v2")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloV2() {
+        return message_v2;
+    }
+
 }
